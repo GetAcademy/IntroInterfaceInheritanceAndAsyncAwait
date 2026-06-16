e6d65b632aee8bc83c29b8e74b5b3fcbc1e61373
GET Academy <terje@kolderup.net>
2026-06-16 09:59:14 +0200

    x

---
+++ TextElements/4_CompositionOverInheritance/Code.cs
@@ -0,0 +1,56 @@
+﻿namespace TextElements._4_CompositionOverInheritance
+{
+    public interface ITextFormatter
+    {
+        string Format(string text);
+    }
+
+    public class HeaderFormatter : ITextFormatter
+    {
+        public string Format(string text)
+        {
+            return $"=== {text.ToUpper()} ===";
+        }
+    }
+
+    public class BulletPointFormatter : ITextFormatter
+    {
+        public string Format(string text)
+        {
+            return $"- {text}";
+        }
+    }
+
+    public class ParenthesesFormatter : ITextFormatter
+    {
+        public string Format(string text)
+        {
+            return $"({text})";
+        }
+    }
+
+    public class TextElement
+    {
+        private readonly string _text;
+        private readonly ITextFormatter _formatter;
+
+        public TextElement(string text, ITextFormatter formatter)
+        {
+            _text = text;
+            _formatter = formatter;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(_formatter.Format(_text));
+        }
+    }
+
+    var title = new TextElement("Velkommen", new HeaderFormatter());
+    var bullet = new TextElement("Lær interface", new BulletPointFormatter());
+    var note = new TextElement("Dette er viktig", new ParenthesesFormatter());
+
+    title.Show();
+    bullet.Show();
+    note.Show();
+}
---
+++ TextElements/4_CompositionOverInheritance/Demo.cs
@@ -0,0 +1,16 @@
+﻿namespace TextElements._4_CompositionOverInheritance
+{
+    internal class Demo
+    {
+        public static void Run()
+        {
+            var title = new TextElement("Velkommen", new HeaderFormatter());
+            var bullet = new TextElement("Lær interface", new BulletPointFormatter());
+            var note = new TextElement("Dette er viktig", new ParenthesesFormatter());
+
+            title.Show();
+            bullet.Show();
+            note.Show();
+        }
+    }
+}
