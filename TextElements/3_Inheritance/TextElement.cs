960bae0e05adf17baed2df371b0e79812b4c8a59
GET Academy <terje@kolderup.net>
2026-06-16 11:16:49 +0200

    Update TextElement.cs

--- TextElements/3_Inheritance/TextElement.cs
+++ TextElements/3_Inheritance/TextElement.cs
@@ -15,3 +15,23 @@
         }
     }
 }
+
+/*
+   internal abstract class TextElement
+   {
+       private readonly string _text;
+
+       protected TextElement(string text)
+       {
+           _text = text;
+       }
+
+       public virtual void Show()
+       {
+           Console.WriteLine(_text);
+       }
+
+       public abstract void Terje();
+   }
+ 
+ */
